57fd49e68612304f6486b0221b7856ce9ad82762
Tobias <tobias@email.com>
2023-08-15 16:21:06 -0300

    Implementando FOR

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -3,19 +3,23 @@ print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
 
-chute = int(input("Digite o seu número: "))
+for rodada in range (1, total_de_tentativas+1):
+#    print("Tentativa: ", rodada, " de ", total_de_tentativas)
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite o seu número: "))
+    print("Você digitou ", chute)
 
-print("Você digitou ", chute)
-acertou = numero_secreto == chute
-maior = numero_secreto < chute
+    acertou = numero_secreto == chute
+    maior = numero_secreto < chute
 
-if(acertou):
-    print ("você acertou")
-    print("Finalizar")
-else:
-    print("você errou")
-    if(maior):
-        print("Chute maior que numero secreto")
+    if(acertou):
+        print ("você acertou")
     else:
-        print("Chute menor que numero secreto")
+        print("você errou")
+        if(maior):
+            print("Chute maior que numero secreto")
+        else:
+            print("Chute menor que numero secreto")
+print("Finalizar")
