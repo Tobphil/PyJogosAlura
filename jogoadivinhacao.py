ae64ffc4c81f7085a07ebbd9728d35c6128c8b9c
Tobias <tobias@email.com>
2023-08-16 19:06:08 -0300

    Jogo1 = pontuação

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -4,29 +4,48 @@ print("********************************")
 print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
-numero_secreto = round(random.randrange(1,101))
-total_de_tentativas = 3
+numero_secreto = round(random.randrange(1, 101))
 print("Número randomico =", numero_secreto)
 
-for rodada in range (1, total_de_tentativas+1):
+# definindo nível dificuldade INICIO
+total_de_tentativas = 0
+print("Escolha o nível dificuldade")
+print("(1) Fácil (2) Médio (3) Difícil")
+dificuldade = int(input("Digite: "))
+if (dificuldade == 1):
+    total_de_tentativas = 20
+elif (dificuldade == 2):
+    total_de_tentativas = 10
+elif (dificuldade == 3):
+    total_de_tentativas = 5
+else:
+    print("Opção inválida") #vai automaticamente para o fim (não atende FOR)
+# definindo nível dificuldade FIM
 
-#    print("Tentativa: ", rodada, " de ", total_de_tentativas)
+#Pontuação jogador que diminui a cada tentativa. Inicio em 100, muda com grau dificuldade
+pontos = 100
+
+for rodada in range(1, total_de_tentativas + 1):
+
+    #print("Tentativa: ", rodada, " de ", total_de_tentativas)
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute = int(input("Digite o seu número entre 1 e 100: "))
-
     acertou = numero_secreto == chute
     maior = numero_secreto < chute
-    if(chute < 1 or chute > 100):
-        print("Você deve digitar um número entre 1 e 100")
-        continue #volta para rodada/ continua execução
 
-    if(acertou):
-        print ("você acertou")
-        break #sai do laço FOR ou While
+    #testa número digitado INICIO
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue  # volta para rodada/ continua execução
+    if (acertou):
+        print("você acertou")
+        break  # sai do laço FOR ou While
+    if (maior):
+        print("Você errou! Chute MAIOR que número secreto")
+        pontos = (pontos - (pontos // total_de_tentativas))
     else:
-        print("você errou")
-        if(maior):
-            print("Chute maior que numero secreto")
-        else:
-            print("Chute menor que numero secreto")
-print("Finalizar")
+        print("Você errou! Chute MENOR que número secreto")
+        pontos = (pontos - (pontos // total_de_tentativas))
+
+print("  ***  FIM  *** ")
+print(" **** Pontos *** {:2d}".format(pontos))
