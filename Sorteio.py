599263e544899cd2b1124903fda03c87ad3d0dd3
Tobias <tobias@email.com>
2023-08-16 17:13:04 -0300

    Jogo 2

---
+++ Sorteio.py
@@ -0,0 +1,12 @@
+import random
+
+numero_sorte = random.randrange(1,4)
+
+print(numero_sorte)
+
+if(numero_sorte == 1):
+    print("Vencedor = Paulo")
+elif(numero_sorte == 2):
+    print("Vencedor: Juliana")
+elif(numero_sorte == 3):
+    print("Vencedor: Tamires")
