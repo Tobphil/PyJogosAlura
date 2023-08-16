5682b6c99549ca5486895afc67bee5eb42d01db4
Tobias <tobias@email.com>
2023-08-16 16:57:55 -0300

    Incluindo função random

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -1,11 +1,15 @@
+import random
+
 print("********************************")
 print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
-numero_secreto = 42
+numero_secreto = round(random.randrange(1,101))
 total_de_tentativas = 3
+print("Número randomico =", numero_secreto)
 
 for rodada in range (1, total_de_tentativas+1):
+
 #    print("Tentativa: ", rodada, " de ", total_de_tentativas)
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute = int(input("Digite o seu número entre 1 e 100: "))
